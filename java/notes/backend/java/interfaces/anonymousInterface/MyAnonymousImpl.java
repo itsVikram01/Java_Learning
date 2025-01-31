5ad4013c6a50c3ecb49bc501bc52ba789340db41
itsVikram01 <vikyrajvikram95@gmail.com>
2025-02-01 04:35:04 +0530

    Interface notes changes

---
+++ java/notes/backend/java/interfaces/anonymousInterface/MyAnonymousImpl.java
@@ -0,0 +1,26 @@
+package notes.backend.java.interfaces.anonymousInterface;
+
+interface MyAnonymousInterface {
+    // anonymous interface can have 2 or more than 2 methods unlike functional interface which have only one abstract method.
+    void switchOn();
+
+    void switchOff();
+}
+
+public class MyAnonymousImpl implements MyAnonymousInterface {
+    public static void main(String[] args) {
+        MyAnonymousInterface myAnonymous = new MyAnonymousImpl();
+        myAnonymous.switchOn();
+        myAnonymous.switchOff();
+    }
+
+    @Override
+    public void switchOn() {
+        System.out.println("Switching on");
+    }
+
+    @Override
+    public void switchOff() {
+        System.out.println("Switching off");
+    }
+}
